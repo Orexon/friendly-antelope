6446892456f00eaa1a7475050729e092d0256d9c
Mind <mindkriauciunas@gmail.com>
2021-12-07 14:04:23 +0200

    InitialFrontEndStructure

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './Components/Authors/authors.component';
+import { BooksComponent } from './Components/Books/books.component';
+import { HomeComponent } from './Components/Home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, BooksComponent, AuthorsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
