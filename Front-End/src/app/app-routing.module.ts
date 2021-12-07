ab8c584818920afaf509823f3361fa22e5b8f1c2
Mind <mindkriauciunas@gmail.com>
2021-12-07 14:15:09 +0200

    CustomNotFound/AppRouting

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorsComponent } from './Components/Authors/authors.component';
+import { BooksComponent } from './Components/Books/books.component';
+import { HomeComponent } from './Components/Home/home.component';
+import { NotFoundComponent } from './Components/NotFound/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Authors', component: AuthorsComponent },
+  { path: 'Books', component: BooksComponent },
+  { path: '404', component: NotFoundComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { AuthorsComponent } from './Components/Authors/authors.component';
 import { BooksComponent } from './Components/Books/books.component';
 import { HomeComponent } from './Components/Home/home.component';
+import { NotFoundComponent } from './Components/NotFound/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, BooksComponent, AuthorsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    AuthorsComponent,
+    NotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
